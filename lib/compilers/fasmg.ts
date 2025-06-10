b06a6e2f6640589afcd849cd9d5290652db9ba1f
fredrik-hjarner <35084243+fredrik-hjarner@users.noreply.github.com>
2025-06-10 16:20:30 +0200

    disable a lot of settings.

--- lib/compilers/fasmg.ts
+++ lib/compilers/fasmg.ts
@@ -12,6 +12,36 @@ export class FasmgCompiler extends BaseCompiler {
     constructor(compilerInfo: PreliminaryCompilerInfo, env: CompilationEnvironment) {
         super(compilerInfo, env);
         this.includePath = this.compilerProps<string | undefined>(`compiler.${this.compiler.id}.includePath`);
+
+        // "supports" settings
+        this.compiler.supportsAsmDocs = false;
+        this.compiler.supportsDeviceAsmView = false;
+        this.compiler.supportsDemangle = false;
+        this.compiler.supportsVerboseDemangling = false;
+        this.compiler.supportsBinary = false;
+        this.compiler.supportsExecute = false;
+        this.compiler.supportsGccDump = false;
+        this.compiler.supportsFiltersInBinary = false;
+        this.compiler.supportsOptOutput = false;
+        this.compiler.supportsVerboseAsm = false;
+        this.compiler.supportsStackUsageOutput = false;
+        this.compiler.supportsPpView = false;
+        this.compiler.supportsAstView = false;
+        this.compiler.supportsIrView = false;
+        this.compiler.supportsClangirView = false;
+        this.compiler.supportsRustMirView = false;
+        this.compiler.supportsRustMacroExpView = false;
+        this.compiler.supportsRustHirView = false;
+        this.compiler.supportsHaskellCoreView = false;
+        this.compiler.supportsHaskellStgView = false;
+        this.compiler.supportsHaskellCmmView = false;
+        this.compiler.supportsCfg = false;
+        this.compiler.supportsGnatDebugViews = false;
+        this.compiler.supportsLibraryCodeFilter = false;
+        this.compiler.supportsMarch = false;
+        this.compiler.supportsTarget = false;
+        this.compiler.supportsTargetIs = false;
+        this.compiler.supportsHyphenTarget = false;
     }
 
     override optionsForFilter(filters, outputFilename) {
